1e02fa87108d5a064a6b174ec06fd2287965c12a
Chie <akiba.chie@gmail.com>
2016-04-28 12:10:18 -0700

    Added keydown keyup function

--- js/app.js
+++ js/app.js
@@ -20,16 +20,30 @@ $(document).ready(function(){
 			$(this).css('left', '551px');
 		}
 	);
-		//show hadouken and animate it to the right of the screen
+	//show hadouken and animate it to the right of the screen
 	})
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 		//ryu goes back to his ready position
-	});
+	})
 });
+
 function playHadouken () {
 	$('#hadouken-sound')[0].volume = 0.5;
 	$('#hadouken-sound')[0].load();
 	$('#hadouken-sound')[0].play();
 }
+
+$(document).on('keydown', function(e) {
+		if (e.keyCode === 88) {
+			$('.ryu-still').hide();
+			$('.ryu-cool').show();		
+		}
+	})
+	.on('keyup', function(e) {
+		if (e.keyCode === 88) {
+			$('.ryu-cool').hide();
+			$('.ryu-still').show();
+		}
+	});
