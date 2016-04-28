e160cbd056f752bac5fe0bfd87598e8996f9a927
Chie <akiba.chie@gmail.com>
2016-04-28 13:03:59 -0700

    Fixed keydown and mouseover issue. Ryu wasn't showing in the right spot when mouseover event was triggered

--- js/app.js
+++ js/app.js
@@ -29,21 +29,22 @@ $(document).ready(function(){
 	})
 });
 
+$(document).keydown(function(e){
+		if(e.keyCode == 88){
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-cool').show();
+        }
+    })
+	.keyup(function(e){
+		if(e.keyCode == 88){
+			$('.ryu-cool').hide();
+			$('.ryu-still').show();
+		}
+    });
+
 function playHadouken () {
 	$('#hadouken-sound')[0].volume = 0.5;
 	$('#hadouken-sound')[0].load();
 	$('#hadouken-sound')[0].play();
 }
-
-$(document).on('keydown', function(e) {
-		if (e.keyCode === 88) {
-			$('.ryu-still').hide();
-			$('.ryu-cool').show();		
-		}
-	})
-	.on('keyup', function(e) {
-		if (e.keyCode === 88) {
-			$('.ryu-cool').hide();
-			$('.ryu-still').show();
-		}
-	});
